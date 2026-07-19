4ce908ea817ae6186eb583e23b18647ea213488a
oruamehhappiness-eng <oruamehhappiness@gmail.com>
2026-07-20 00:58:20 +0100

    Update int-database.sql

--- scripts/int-database.sql
+++ scripts/int-database.sql
@@ -19,6 +19,7 @@ GO
   IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
   BEGIN
     ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE Datawarehouse;
 END;
 GO 
   
